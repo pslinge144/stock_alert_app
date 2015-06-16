89421ff8ece9d67736823f67a778a47c0cdfff9d
Philip Slingerland <pslinge144@gmail.com>
2015-06-15 21:25:02 -0400

    Added AndRule to rule.py and addded unit tests

--- src/stock_alerter/rule.py
+++ src/stock_alerter/rule.py
@@ -16,3 +16,11 @@ class PriceRule:
 
 	def depends_on(self):
 		return {self.symbol}
+
+class AndRule:
+
+	def __init__(self, *args):
+		self.rules = args
+
+	def matches(self, exchange):
+		return all([rule.matches(exchange) for rule in self.rules])
