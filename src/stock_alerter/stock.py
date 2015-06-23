0593a00f72f4d5f845a8f9968035650f0873315a
Philip Slingerland <pslinge144@gmail.com>
2015-06-22 20:16:34 -0400

    Completed method extraction from Stock into TimeSeries class

--- src/stock_alerter/stock.py
+++ src/stock_alerter/stock.py
@@ -1,12 +1,7 @@
 import bisect
-import collections
 from .timeseries import TimeSeries
 from datetime import timedelta
 from enum import Enum
-
-
-PriceEvent = collections.namedtuple("PriceEvent",
-	["timestamp", "price"])
 
 
 class StockSignal(Enum):
@@ -21,7 +16,6 @@ class Stock:
 
 	def __init__(self, symbol):
 		self.symbol = symbol
-		self.price_history = []
 		self.history = TimeSeries()
 
 	@property
@@ -34,28 +28,12 @@ class Stock:
 	def update(self, timestamp, price):
 		if price < 0:
 			raise ValueError("price should not be negative")
-		bisect.insort_left(self.price_history, PriceEvent(timestamp, price))
 		self.history.update(timestamp, price)
 
 	def is_increasing_trend(self):
 		return self.history[-3].value < \
 			self.history[-2].value < \
 				self.history[-1].value
-
-	def _get_closing_price_list(self, on_date, num_days):
-		closing_price_list = []
-		for i in range(num_days):
-			chk = on_date.date() - timedelta(i)
-			for price_event in reversed(self.price_history):
-				if price_event.timestamp.date() > chk:
-					pass
-				if price_event.timestamp.date() == chk:
-					closing_price_list.insert(0, price_event)
-					break
-				if price_event.timestamp.date() < chk:
-					closing_price_list.insert(0, price_event)
-					break
-		return closing_price_list
 
 
 	def _is_crossover_below_to_above(self, prev_ma,
@@ -69,7 +47,7 @@ class Stock:
 	def get_crossover_signal(self, on_date):
 		NUM_DAYS = self.LONG_TERM_TIMESPAN + 1
 		closing_price_list = \
-			self._get_closing_price_list(on_date, NUM_DAYS)
+			self.history.get_closing_price_list(on_date, NUM_DAYS)
 
 		if len(closing_price_list) < NUM_DAYS:
 			return StockSignal.neutral
@@ -81,16 +59,16 @@ class Stock:
 		prev_short_term_series = \
 			closing_price_list[-self.SHORT_TERM_TIMESPAN-1:-1]
 
-		long_term_ma = sum([update.price
+		long_term_ma = sum([update.value
 				    for update in long_term_series])\
 				/self.LONG_TERM_TIMESPAN
-		prev_long_term_ma = sum([update.price
+		prev_long_term_ma = sum([update.value
 				    for update in prev_long_term_series])\
 				     /self.LONG_TERM_TIMESPAN
-		short_term_ma = sum([update.price
+		short_term_ma = sum([update.value
 				     for update in short_term_series])\
 				/self.SHORT_TERM_TIMESPAN	
-		prev_short_term_ma = sum([update.price
+		prev_short_term_ma = sum([update.value
 					  for update in prev_short_term_series])\
 				     /self.SHORT_TERM_TIMESPAN	
 
