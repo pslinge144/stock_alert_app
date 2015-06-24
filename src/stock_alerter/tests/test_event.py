4d4e52511cc7f8632179fa1879ce29c11c4b0de4
slingerland <slingerland@metsci.com>
2015-06-24 12:48:10 -0400

    Implemented simple Event class along with a test class. Test class contains a naive mock for an event listener.

---
+++ src/stock_alerter/tests/test_event.py
@@ -0,0 +1,28 @@
+import unittest
+from ..event import Event
+
+
+class Mock:
+	def __init__(self):
+		self.called = False
+		self.params = ()
+	
+	def __call__(self, *args, **kwargs):
+		self.called = True
+		self.params = (args, kwargs)
+
+
+class EventTest(unittest.TestCase):
+	def test_a_listener_is_notified_when_an_event_is_raised(self):
+		listener = Mock()
+		event = Event()
+		event.connect(listener)
+		event.fire()
+		self.assertTrue(listener.called)
+
+	def test_a_listener_is_passed_right_parameters(self):
+		listener = Mock()
+		event = Event()
+		event.connect(listener)
+		event.fire(5, shape="square")
+		self.assertEqual(((5, ), {"shape":"square"}), listener.params)
