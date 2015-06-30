62751fa10564dc6e3d01e896223f99f09a73d06c
Philip Slingerland <pslinge144@gmail.com>
2015-06-29 20:22:00 -0400

    Added alert module and a test to verify its funtion using mocks

---
+++ src/stock_alerter/tests/test_alert.py
@@ -0,0 +1,27 @@
+import unittest
+from datetime import datetime
+from unittest import mock
+
+from ..alert import Alert
+from ..rule import PriceRule
+from ..stock import Stock
+
+
+class TestAction:
+	executed = False
+
+	def execute(self, description):
+		self.executed = True
+
+
+class AlertTest(unittest.TestCase):
+	def test_action_is_executed_when_rule_matches(self):
+		exchange = {"GOOG": Stock("GOOG")}
+		rule = mock.MagicMock(spec=PriceRule)
+		rule.matches.return_value = True
+		rule.depends_on.return_value = {"GOOG"}
+		action = mock.MagicMock()
+		alert = Alert("sample alert", rule, action)
+		alert.connect(exchange)
+		exchange["GOOG"].update(datetime(2014, 2, 10), 11)
+		action.execute.assert_called_with("sample alert")
